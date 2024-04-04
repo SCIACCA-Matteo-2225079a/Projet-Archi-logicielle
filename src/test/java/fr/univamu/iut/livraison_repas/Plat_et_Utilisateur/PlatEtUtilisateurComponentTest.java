e43251ba560b0f5e9da903c035b96280acee3917
THIEL-Maxime-2225084a <123471441+THIEL-Maxime-2225084a@users.noreply.github.com>
2024-04-05 00:41:06 +0200

    Update PlatEtUtilisateurComponentTest.java

--- src/test/java/fr/univamu/iut/livraison_repas/Plat_et_Utilisateur/PlatEtUtilisateurComponentTest.java
+++ src/test/java/fr/univamu/iut/livraison_repas/Plat_et_Utilisateur/PlatEtUtilisateurComponentTest.java
@@ -1,4 +1,51 @@
 package fr.univamu.iut.livraison_repas.Plat_et_Utilisateur;
 
+import static org.junit.Assert.*;
+import org.junit.*;
+import jakarta.enterprise.inject.spi.CDI;
+import jakarta.ws.rs.ApplicationPath;
+
 public class PlatEtUtilisateurComponentTest {
+
+    private PlatEtUtilisateurComponent component;
+
+    @Before
+    public void setComponent() throws Exception {
+        component = new PlatEtUtilisateurComponent();
+    }
+
+    @Test
+    public void testApplicationPath() {
+        ApplicationPath applicationPathAnnotation = PlatEtUtilisateurComponent.class.getAnnotation(ApplicationPath.class);
+        assertNotNull(applicationPathAnnotation);
+        assertEquals("/api", applicationPathAnnotation.value());
+    }
+
+    @Test
+    public void testOpenDbConnectionForPlat() {
+        PlatRepositoryInterface platRepository = component.openDbConnectionForPlat();
+        assertNotNull(platRepository);
+    }
+
+    @Test
+    public void testOpenDbConnectionForUtilisateur() {
+        UtilisateurRepositoryInterface utilisateurRepository = component.openDbConnectionForUtilisateur();
+        assertNotNull(utilisateurRepository);
+    }
+
+    @Test
+    public void testCloseDbConnectionForPlat() {
+        PlatRepositoryInterface platRepository = CDI.current().select(PlatRepositoryInterface.class).get();
+        assertNotNull(platRepository);
+        component.closeDbConnectionForPlat(platRepository);
+    }
+
+    @Test
+    public void testCloseDbConnectionForUtilisateur() {
+        UtilisateurRepositoryInterface utilisateurRepository = CDI.current().select(UtilisateurRepositoryInterface.class).get();
+        assertNotNull(utilisateurRepository);
+        component.closeDbConnectionForUtilisateur(utilisateurRepository);
+    }
+
 }
+
